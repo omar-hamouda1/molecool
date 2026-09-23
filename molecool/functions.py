3799a7dc60d877878d72fddcf7040a172d12e9ea
omar-hamouda1 <omarmohammed20400@outlook.com>
2026-09-23 23:19:09 +0300

    add write_xyz function with ValueError check

--- molecool/functions.py
+++ molecool/functions.py
@@ -69,5 +69,31 @@ def open_pdb(file_location):
 
     coords = np.array(coordinates)
     symbols = np.array(symbols)
-
     return symbols, coords
+
+
+def write_xyz(file_location, symbols, coordinates):
+    """Write an xyz file given a file location, symbols, and coordinates."""
+
+    num_atoms = len(symbols)
+
+    if num_atoms != len(coordinates):
+        raise ValueError(
+            f"write_xyz : the number of symbols ({num_atoms}) "
+            f"and number of coordinates ({len(coordinates)}) "
+            f"must be the same to write xyz file!"
+        )
+
+    with open(file_location, "w+") as f:
+        f.write("{}\n".format(num_atoms))
+        f.write("XYZ file\n")
+
+        for i in range(num_atoms):
+            f.write(
+                "{}\t{}\t{}\t{}\n".format(
+                    symbols[i],
+                    coordinates[i, 0],
+                    coordinates[i, 1],
+                    coordinates[i, 2],
+                )
+            )
